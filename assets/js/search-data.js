e13f1c2bc6aa344b85b59d83ddaa25652c46b36b
suguru-y <165854378+suguru-y@users.noreply.github.com>
2025-08-19 17:46:41 +0000

    Deploying to gh-pages from @ suguru-y/suguru-y.github.io@d7560228d636ef575c79156700f1a23df79679cd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -79,6 +79,9 @@ ninja.data = [{
           section: "News",},{id: "news-this-paper-in-dalton-transactions-selected-as-a-hot-article",
           title: 'This paper in Dalton Transactions selected as a HOT article!',
           description: "",
+          section: "News",},{id: "news-giving-a-seminar-at-the-young-researcher-forum-of-the-ceramics-society-of-japan-kansai-branch-oct-14-15-looking-forward-to-seeing-you-there",
+          title: 'Giving a seminar at the Young Researcher Forum of the Ceramics Society of...',
+          description: "",
           section: "News",},{
         id: 'social-email',
         title: 'email',
